e2cf7bb9994b341ab6268a3dac2cdaee8194a433
tharukabandara2005 <tharuka.sathvidu@gmail.com>
2024-01-31 21:23:45 +0530

    started implementation of the registration page

---
+++ sdgp-roamceylon/src/Components/RegistrationPage/RegistrationPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./RegistrationPage.css";
+
+
+
+export const RegistrationPage = () => {
+  return (
+    <>
+            <div className='header'>
+                <h2>ROAMCEYLON</h2>
+            </div>
+            <div className="login-box">
+                <h2 className="sub-heading">Be a part of the community.</h2>
+                <form action="" className="loginbox"> 
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="First Name" required/>
+                        <i className='bx bxs-user'></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Last Name" required/>
+                        <i className='bx bxs-user'></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Email" required/>
+                        <i className='bx bxs-user'></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="user name" required/>
+                        <i className='bx bxs-user'></i>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required/>
+                        <i className='bx bxs-lock-alt' ></i>
+                    </div>
+
+
+                    <button type="submit" className="login-btn">Create account</button>
+                </form>
+                    <div className="register-account">
+                        <p>Don't have an account, Click here to <a href="#">create a new account</a></p>
+                    </div>
+            </div>
+        </> 
+  )
+}
