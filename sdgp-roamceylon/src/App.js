98a1ee9153e6abf45f102d88281d127cf7c11241
Kikuro <62168060+chiran-commits@users.noreply.github.com>
2024-01-31 10:10:00 +0530

    Fully responsive login page

--- sdgp-roamceylon/src/App.js
+++ sdgp-roamceylon/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import LoginPage from './Components/LoginPage/LoginPage.js';
+import Logoheader from './Components/LoginPage/LogoHeader.js';
 
 function App() {
   return (
     <div>
+      <Logoheader/>
       <LoginPage/>
     </div>  
   );
