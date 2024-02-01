dd50a06c8b634ace1ac4dead09f83b292d6c71c7
Puleesha <puleeshak@gmail.com>
2024-02-01 20:07:25 +0530

    Created the Reviews Page
    Created the ReviewsPage.js file and ReviewsPage.css file

---
+++ sdgp-roamceylon/src/Components/ReviewsPage/ReviewsPage.js
@@ -0,0 +1,16 @@
+import Navbar from "../AboutUsPage/Navbar";
+
+export default function ReviewsPage(){
+    return(
+        <>
+        <div className = "outer-layer">
+            <div className = "middle-layer">
+            <Navbar/>
+            <div className = "inner-layer">
+                
+            </div>
+            </div>
+        </div>
+        </>
+    );
+}
