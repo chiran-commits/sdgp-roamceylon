e00391722d2bd95b399403fa12df42b1f835d6f7
tharukabandara2005 <tharuka.sathvidu@gmail.com>
2024-02-02 23:04:06 +0530

    update registrationPage.js

--- sdgp-roamceylon/src/Components/RegistrationPage/RegistrationPage.js
+++ sdgp-roamceylon/src/Components/RegistrationPage/RegistrationPage.js
@@ -6,8 +6,41 @@ import React, { useState, useEffect } from 'react';
 
 
 
+
 export const RegistrationPage = () => {
+
+
     const [loading, setLoading] = useState(true);
+
+    const [data,setData]=useState({
+        firstName:'',lastName:"",email:'',password:''
+    })
+
+    const registerUser = (e) => {
+        e.preventDefault()
+        const {name,email,password}=data
+        try{
+            
+            const res=await fetch('/register',{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    name,email,password
+                })
+            })
+
+            
+            
+
+        }catch (err) {
+            console.log(err);
+        }
+            
+       
+
+    }
 
     useEffect(() => {
   
@@ -24,15 +57,15 @@ export const RegistrationPage = () => {
             </div>
             <div className="login-box">
                 <h2 className="sub-heading">Be a part of the community.</h2>
-                <form action="" className="loginbox"> 
+                <form  className="loginbox" onSubmit={registerUser}> 
                     <h1>register</h1>
                     <div>
                         <div className="input-box">
-                            <input type="text" placeholder="First Name" required/>
+                            <input type="text" placeholder="First Name" required  value={data.firstName} onChange={(e)=>setData({...data,firstName:e.target.value})}/>
                             <i className='bx bxs-user'></i>
                         </div>
                         <div className="input-box">
-                            <input type="text" placeholder="Last Name" required/>
+                            <input type="text" placeholder="Last Name" required  value={data.lastName} onChange={(e)=>setData({...data,lastName:e.target.value})}/>
                             <i className='bx bxs-user'></i>
                         </div>
 
@@ -40,15 +73,11 @@ export const RegistrationPage = () => {
                 
                     
                     <div className="input-box">
-                        <input type="text" placeholder="Email" required/>
+                        <input type="text" placeholder="Email" required value={data.email} onChange={(e)=>setData({...data,email:e.target.value})}/>
                         <i className='bx bxs-user'></i>
                     </div>
                     <div className="input-box">
-                        <input type="text" placeholder="user name" required/>
-                        <i className='bx bxs-user'></i>
-                    </div>
-                    <div className="input-box">
-                        <input type="password" placeholder="Password" required/>
+                        <input type="password" placeholder="Password" required value={data.password} onChange={(e)=>setData({...data,password:e.target.value})}/>
                         <i className='bx bxs-lock-alt' ></i>
                     </div>
 
