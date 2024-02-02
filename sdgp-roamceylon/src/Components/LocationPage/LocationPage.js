d47f6afd1eef105d945436ad3e828de5cda31e04
Puleesha <puleeshak@gmail.com>
2024-02-02 19:51:34 +0530

    Changes made to location page

--- sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
+++ sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
@@ -5,10 +5,17 @@ export default function LocationPage(){
         <>
         <div className = "outer-layer">
             <div className = "middle-layer">
-            <Navbar/>
-            <div className = "inner-layer">
-                
-            </div>
+                <Navbar/>
+                <div className = "inner-layer">
+                    <div className = "Manual-search">
+                        <h1>Manual Search</h1>
+                        <input type = "text" placeholder = "Enter location name"/>
+                    </div>
+                    <div className = "Recommendation">
+                        <h1>Receive Recommendations</h1>
+                        <input type = "text" placeholder = "Enter a description of your preferred locations"/>
+                    </div>
+                </div>
             </div>
         </div>
         </>
