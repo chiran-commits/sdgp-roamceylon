9ca1732f56ef5eef9d9a56771791f21a3936ba72
Puleesha <puleeshak@gmail.com>
2024-02-05 11:42:54 +0530

    Changes to the Location Page

--- sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
+++ sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
@@ -8,10 +8,18 @@ export default function LocationPage(){
             <div className="mainContainer">
                 <div className="container">
                     <div className="leftContainer">
-
+                        <h2>Manual Search</h2>
+                        <div className = "textbox">
+                            <input type = "text" placeholder = "Search the location"/>
+                        </div>
+                        <button type = "submit">Search Location</button>
                     </div>
                     <div className="rightContainer">
-
+                        <h2>Provide Recommandation</h2>
+                        <div className = "textbox">
+                            <input type = "text" placeholder = "Provide a description of your preferred place"/>
+                        </div>
+                        <button type = "submit">Find your Location</button>
                     </div>
                 </div>
             </div>
