7cfa6d562f35562ced51028675bcf979304067e9
Thinal Karunarathna <118673052+thinalk1@users.noreply.github.com>
2024-02-06 16:14:02 +0530

    CSS Changes to the Location Page

--- sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
+++ sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import Navbar from "../AboutUsPage/Navbar";
 import "./LocationPage.css";
+import leftimage from './leftimage.jpg';
+import rightimage from './rightimage.jpg';
 
 export default function LocationPage(){
     return(
@@ -9,10 +12,12 @@ export default function LocationPage(){
                 <div className="container">
                     <div className="leftContainer">
                         <h2><center>Manual Search</center></h2>
+                        <img src={leftimage} className='locationImages'/>
                     </div>
                     
                     <div className="rightContainer">
                         <h2><center>Provide Recommandation</center></h2>
+                        <img src={rightimage} className='locationImages'/>
                     </div> 
                 </div>
             </div>
