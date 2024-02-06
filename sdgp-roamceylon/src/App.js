35539dccf190fd7dc77810c4b73a3cc076de1251
Puleesha <puleeshak@gmail.com>
2024-02-06 09:25:25 +0530

    Minor changes

--- sdgp-roamceylon/src/App.js
+++ sdgp-roamceylon/src/App.js
@@ -10,12 +10,8 @@ import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 function App() {
 
-
-
-
   return (
-  
-  
+
   <Router>
       <Routes>
         <Route path="/" element={<LoginPage/>} />
