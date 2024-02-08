82a4bb7c118d540707bd6dea1979f8300808c6f9
Thinal Karunarathna <118673052+thinalk1@users.noreply.github.com>
2024-02-08 12:10:49 +0530

    Updated the Navigation Bar

--- sdgp-roamceylon/src/Components/AboutUsPage/Navbar.js
+++ sdgp-roamceylon/src/Components/AboutUsPage/Navbar.js
@@ -1,16 +1,22 @@
 import "./NavbarStyles.css";
 import user from "./images.png"
+import  React from 'react';
+import homePage from '../HomePage/HomePage.js';
+import '../LocationPage/LocationPage.js';
+import aboutusPage from '../AboutUsPage/AboutUsPage.js';
+import userProfile from '../UserProfile/UserProfile.js';
 
-function Navbar(){
+
+const Navbar = ({activeOption}) =>{
    return(
     <nav>
-        <a id="moveToRight" href="index.html"><h2>ROAMCEYLON</h2></a>
+        <a id="moveToRight" href="/"><h2>ROAMCEYLON</h2></a>
         <div>
             <ul id="navbar">
-                <li><a href="index.html">HOME</a></li>
-                <li><a href="#">LOCATIONS</a></li>
-                <li><a className="active" href="#">ABOUT US</a></li>
-                <li><a href="index.html"><img className="userimg" src={user}/></a></li>
+                <li><a href="/" className={activeOption == 'home' ? 'active' : ''}>HOME</a></li>
+                <li><a href="/location" className={activeOption == 'locations' ? 'active' : ''}>LOCATIONS</a></li>
+                <li><a href="/about" className={activeOption == 'aboutus' ? 'active' : ''}>ABOUT US</a></li>
+                <li><a href="profile"><img className="userimg" src={user}/></a></li>
             </ul>
         </div>
     </nav>
