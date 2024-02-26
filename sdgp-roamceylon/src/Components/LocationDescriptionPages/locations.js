071af8f3821784c55356f336ca649f6781143e87
Puleesha <puleeshak@gmail.com>
2024-02-26 12:42:23 +0530

    Create locations.js

---
+++ sdgp-roamceylon/src/Components/LocationDescriptionPages/locations.js
@@ -0,0 +1,15 @@
+[{
+    "id":1,
+    "location":"Colombo",
+    "wifi":"AVERAGE",
+    "col":"LOW COST",
+    "type":"URBAN"
+},
+{
+    "id":2,
+    "location":"Galle",
+    "wifi":"AVERAGE",
+    "col":"HIGH COST",
+    "type":"RURAL"
+}
+]
