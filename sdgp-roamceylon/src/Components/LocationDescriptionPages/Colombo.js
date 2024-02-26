b86f3b6801d85c9baab9278ea4f6afa621696c8c
Puleesha <puleeshak@gmail.com>
2024-02-26 12:40:13 +0530

    Changes made to colombo.js

--- sdgp-roamceylon/src/Components/LocationDescriptionPages/Colombo.js
+++ sdgp-roamceylon/src/Components/LocationDescriptionPages/Colombo.js
@@ -1,36 +1,164 @@
 import React from 'react'
 import Navbar from '../AboutUsPage/Navbar';
 import './colombo.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import LocationData from './locations.json'
 
 const Colombo = () => {
-  return (
-    <div className='colombopage'>
-        <Navbar></Navbar>
-        <section className="colombopagehero">
-            <div className="colombopagetext-container">
-                {/* <div class="top-part"></div> */}
-                <h1>Colombo</h1>
-                <p>Nestled along the western coast of Sri Lanka, Colombo stands as a vibrant metropolis that seamlessly blends rich history with modernity. This bustling capital city is not just a destination; it's an experience for digital nomads seeking adventure, culture, and inspiration.</p>
-                <div className="colombopageindicators">
-                    <p>WEATHER: TROPICAL</p>
-                    <p>WIFI: High-Speed</p>
-                    <p>COST OF LIVING: Medium</p>
-                    <p>URBAN/RURAL: URBAN</p>
+
+    const [review, setReview] = useState('');
+    const [rating, setRating] = useState(0);
+    const location = "galle"
+    const name = "tharuka"
+    const [publisedReviews, setPublisedReviews] = useState([]);
+
+    const [pressedSubmit, setPressedSubmit] = useState(false);
+
+
+
+    const handleReviewChange = (e) => {
+        setReview(e.target.value);
+    };
+
+
+    useEffect(() => {
+        const getReviwes = async () => {
+            try {
+                let publisedReviews = await axios.get(`http://localhost:5009/review?location=${location}`, { location }).catch(
+                    console.log("error fetching data")
+                )
+                console.log(publisedReviews.data)
+                let publishedReviewsArray = Object.values(publisedReviews.data);
+                setPublisedReviews(publishedReviewsArray);
+
+            }
+            catch (err) {
+                console.log(err)
+            }
+
+
+
+
+
+        }
+        getReviwes()
+
+    }, [pressedSubmit])
+
+
+
+
+
+    const handleRatingChange = (e) => {
+        setRating(parseInt(e.target.value));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+
+        await axios.post('http://localhost:5009/review', { name, location, review, rating }).catch((err) => {
+            console.log(err)
+            setReview('');
+            setRating(0);
+        })
+        setPressedSubmit(!pressedSubmit);
+        setReview('');
+        setRating(0);
+    };
+
+
+
+
+    return (
+        <div className='colombopage'>
+            <Navbar></Navbar>
+
+            <section className="colombopagehero">
+                <div className="colombopagetext-container">
+                    <h2>Colombo</h2>
+                    {
+                        LocationData.map(data => {
+                            if (data.location == "Colombo") {
+                                return (
+                                    <div className="colombopageindicators">
+                                        <p>WEATHER: TROPICAL</p>
+                                        <p>WIFI: {data.wifi}</p>
+                                        <p>COST OF LIVING: {data.col}</p>
+                                        <p>URBAN/RURAL: {data.type}</p>
+                                    </div>
+                                );
+                            }
+                        })
+                    }
                 </div>
-            </div>
-        </section>
-        <section className="colombopagereviews">
-            <h3>REVIEWS</h3>
-            <div className="colombopagereview">
-                <h2>Jake</h2>
-                <p>Nunc sit amet condimentum metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus viverra efficitur diam eget fringilla. Donec euismod eleifend massa non volutpat. Lorem ipsum do olor sit amet, consectetur adipiscing elit. Duis mattis quis dolor a rhoncus. Sed eros magna, venenatis et urna vitoe, vehicula porttitor nulla.</p>
-            </div>
-        </section>
-   
+                <div>
+
+                    <div className="form-container">
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label>
+                                    Review:
+                                    <textarea
+                                        value={review}
+                                        onChange={handleReviewChange}
+                                        required
+                                    />
+                                </label>
+                            </div>
+                            <div>
+
+                                <label>
+                                    Rating:
 
 
-    </div>
-  )
+                                </label>
+                                <div class="rating">
+                                    <input value="5" name="rating" id="star5" type="radio" checked={rating === 5} onChange={handleRatingChange} />
+                                    <label for="star5"></label>
+                                    <input value="4" name="rating" id="star4" type="radio" checked={rating === 4} onChange={handleRatingChange} />
+                                    <label for="star4"></label>
+                                    <input value="3" name="rating" id="star3" type="radio" checked={rating === 3} onChange={handleRatingChange} />
+                                    <label for="star3"></label>
+                                    <input value="2" name="rating" id="star2" type="radio" checked={rating === 2} onChange={handleRatingChange} />
+                                    <label for="star2"></label>
+                                    <input value="1" name="rating" id="star1" type="radio" checked={rating === 1} onChange={handleRatingChange} />
+                                    <label for="star1"></label>
+                                </div>
+
+                            </div>
+                            <button type="submit">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </section>
+            <section className="colombopagereviews">
+                <h3>REVIEWS</h3>
+                <div className="colombopagereview">
+                    <h2>Jake</h2>
+                    <ul className="comments-list">
+                        {publisedReviews.map((comment) => (
+                            <li key={comment._id} className="comment-item">
+                                <h3>{comment.name}</h3>
+                                <p>{comment.review}</p>
+                                <p>{comment.rating}</p>
+
+                            </li>
+                        ))}
+                    </ul>
+
+
+                </div>
+            </section>
+
+
+
+        </div>
+    )
 }
 
 export default Colombo
+
+
