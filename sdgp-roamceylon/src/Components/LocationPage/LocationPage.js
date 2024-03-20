ca849acb38a316ee5193ca681200a5736fcb40b6
Thinal Karunarathna <118673052+thinalk1@users.noreply.github.com>
2024-03-20 23:04:47 +0530

    minor changes

--- sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
+++ sdgp-roamceylon/src/Components/LocationPage/LocationPage.js
@@ -129,7 +129,7 @@ export default function LocationPage() {
                             )}
                         </div>
                     </div>
-                    {isLoggedIn && (
+                    { true/*isLoggedIn */ && (
                         <div className="rightContainer">
                             <h2><center>Provide Recommendation</center></h2>
                             <div className='textarea-center'>
