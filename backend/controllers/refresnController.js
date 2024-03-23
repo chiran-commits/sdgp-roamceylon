bed35124263b14c0c2f0e439bc302b64294e9b97
tharukabandara2005 <tharuka.sathvidu@gmail.com>
2024-03-23 18:02:23 +0530

    remove unwanted files

--- backend/controllers/refresnController.js
+++ backend/controllers/refresnController.js
@@ -1,34 +1,29 @@
-const userModel= require('../database/userModel');
+// const userModel= require('../database/userModel');
 
-const refresh=async(req,res)=>{
+// const refresh=async(req,res)=>{
+//     // const cookies = req.cookies;
+//     // if (!cookies?.jwt) return res.sendStatus(401);
+//     // const refreshToken = cookies.jwt;
 
+//     if (!req.cookies.token) return res.sendStatus(401);
+//     const refreshToken = req.cookies.token;
 
-    // const cookies = req.cookies;
-    // if (!cookies?.jwt) return res.sendStatus(401);
-    // const refreshToken = cookies.jwt;
+//         jwt.verify(
+//             refreshToken,
+//             process.env.REFRESH_TOKEN_SECRET,
+//             (err, decoded) => {
 
-    if (!req.cookies.token) return res.sendStatus(401);
-    const refreshToken = req.cookies.token;
+//                 if (err) return res.sendStatus(403);
 
+//                 const accessToken = jwt.sign(
+//                     { "username": decoded.username },
+//                     process.env.ACCESS_TOKEN_SECRET,
+//                     { expiresIn: '30s' }
+//                 );
+//                 res.json({ accessToken })
+//             }
+//         );
 
-    
+// }
 
-        jwt.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            (err, decoded) => {
-
-                if (err) return res.sendStatus(403);
-
-                const accessToken = jwt.sign(
-                    { "username": decoded.username },
-                    process.env.ACCESS_TOKEN_SECRET,
-                    { expiresIn: '30s' }
-                );
-                res.json({ accessToken })
-            }
-        );
-
-}
-
-module.exports=refresh;
+// module.exports=refresh;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router =express.Router();
-const axios = require('axios');
 const verifyUser = require('../middleware/verifyJWT');
 
 const {registerUser, loginUser} = require('../controllers/registerController');
 const {addReview,sendReview}= require('../controllers/reviewController');
-const refresh= require('../controllers/refresnController');
- 
-
 
 const saveUser=require('../controllers/saveController');
 const {sendUser}=require('../controllers/usersController');
@@ -27,7 +23,7 @@ router.patch('/save',saveUser)
 router.get("/review",sendReview)
 router.get('/user',verifyUser,sendUser)
 
-router.get('/refresh',refresh)
+
 
 
 
